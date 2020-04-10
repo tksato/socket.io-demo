4b7475a9e973389706e751ed2bfec7442f9052ae
tksato <takayukisatou19801015@gmail.com>
2020-04-10 14:20:30 +0900

    fix event args

--- client/app.js
+++ client/app.js
@@ -3,7 +3,7 @@
  */
 
 const options = {
-    'reconnectionAttempts':10,
+    'reconnectionAttempts': 10,
     'reconnectionDelay': 100,
     'reconnectionDelayMax': 300,
     'timeout': 5000,
@@ -12,7 +12,6 @@ const options = {
 const config = require('./config.json');
 const client = require('socket.io-client');
 const socket = client.connect(config.host, options);
-let reconnectCount = 0;
 
 socket.on('connect', () => {
     console.log(`client connected host:${config.host}`);
@@ -22,29 +21,28 @@ socket.on('message', (data) => {
     console.log(`message：${data}`);
 });
 
-socket.on('connect_error', () => {
-    console.log('connect_error.');
+socket.on('connect_error', (error) => {
+    console.log(`connect_error.${JSON.stringify(error)}`);
 });
 
 socket.on('connect_timeout', () => {
     console.log('connect_timeout.');
 });
 
-socket.on('reconnect', () => {
-    console.log('reconnect.');
+socket.on('reconnect', (attempt) => {
+    console.log(`reconnect count:${attempt}`);
 });
 
-socket.on('reconnect_attempt', () => {
-    reconnectCount++;
-    console.log(`reconnect_attempt count:${reconnectCount}`);
+socket.on('reconnect_attempt', (attempt) => {
+    console.log(`reconnect_attempt count:${attempt}`);
 });
 
-socket.on('reconnecting', () => {
-    console.log('reconnecting.');
+socket.on('reconnecting', (attempt) => {
+    console.log(`reconnecting count:${attempt}`);
 });
 
-socket.on('reconnect_error', () => {
-    console.log('reconnect_error.');
+socket.on('reconnect_error', (error) => {
+    console.log(`reconnect_error.${JSON.stringify(error)}`);
 });
 
 socket.on('reconnect_failed', () => {
